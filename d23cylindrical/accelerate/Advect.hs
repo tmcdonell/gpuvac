74cf2c130cb9275fd9e30a2fde1e98bbf9826742
Jonathan Fraser <jonathanfras@gmail.com>
2018-01-21 10:58:57 -0800

    TODO: figure out boundry conditions for all of this

--- d23cylindrical/accelerate/Advect.hs
+++ d23cylindrical/accelerate/Advect.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
-
 module Advect where 
 
 import qualified Prelude as P 
@@ -25,8 +24,6 @@ class (Elt diff, Elt flux) => Accumulate flux diff | diff -> flux where
 class Elt diff => Merge diff  where 
     merge :: Exp diff -> Exp diff -> Exp diff  
 
-class (Elt diff, Elt state) => Integrate diff state | state -> diff where 
-    integrate :: Exp Double -> Exp diff -> Exp state -> Exp state 
 
 type Geom = (V3 (V3 Double, V3 Double),Double) 
 
@@ -52,22 +49,14 @@ advect areas state = zip uflux dflux where
                     (_,uflux) = unzip $ Acc.zipWith flux unorm ustate
                     (dflux,_) = unzip $ Acc.zipWith flux dnorm dstate
 
+--advect1D :: (Interp state, Flux state flux) => Acc (Array DIM1 (V3 (V3 Double,V3 Double))) -> Boundary (Array DIM1 state) -> Acc (Array DIM1 state) -> Acc (Array DIM1 (V1 (flux,flux))) 
+--advect1D geom bound input = fl where
+--                            area1 :: Acc (Array DIM1 (V3 Double, V3 Double))
+--                            area1 = Acc.map (\v -> v^._x) geom
+--                            sten = stencil stencil1D bound 
+--                            st = sten input
+--                            fl1 = advect area1 st
+--                            fl = Acc.map (lift.V1) fl1 
 
---compute the state derivative given a cell geometry and the fluxes
---accumulate :: (Floating a,Advect b) => Geometry a -> VoxelFlux b -> State b
---accumulate geom flux = scale (recip volume) totalflux
---        where 
---            (faceareas,volume) = geom
---            dflux = liftA2 fluxsum faceareas flux 
---            totalflux = sum dflux
 
---two step performs a basic two step time integration
---it takes a geometry over which 
---twostep :: (Floating a,Advect b) => (State b -> VoxelFlux b) -> (State b -> VoxelFlux b) -> Geometry a -> Time a -> State b -> State b
---twostep predict main geom dt start = end  
---        where 
---        du_start = accumulate geom (predict start)
---        mid = start + scale (dt/2) start
---        du_mid = accumulate geom (main mid) 
---        end = start + scale dt du_mid
 
