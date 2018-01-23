4adb3a07259f2eb4e5cc61b6f5b3c75875fc2349
Jonathan Fraser <jonathan.fraser@generalfusion.com>
2018-01-23 13:02:43 -0800

    add boundary cell correction to integration step

--- d23cylindrical/accelerate/Integration.hs
+++ d23cylindrical/accelerate/Integration.hs
@@ -9,11 +9,11 @@ import Data.Array.Accelerate as Acc
 
 import Types 
 
-twostep :: (Shape sh, Elt diff, Elt state) => Accumulator diff state -> (Acc (Array sh state) -> Acc (Array sh diff)) -> (Acc (Array sh state) -> Acc (Array sh diff)) -> Exp Double -> Acc (Array sh state) -> Acc (Array sh state) 
-twostep integrate predict advance time start = final 
+twostep :: (Shape sh, Elt diff, Elt state) => Accumulator diff state -> (Acc (Array sh state) -> Acc (Array sh state)) -> (Acc (Array sh state) -> Acc (Array sh diff)) -> (Acc (Array sh state) -> Acc (Array sh diff)) -> Exp Double -> Acc (Array sh state) -> Acc (Array sh state) 
+twostep integrate boundry predict advance time start = final 
             where
-                step t d u = Acc.zipWith (\di st -> integrate t di st) d u 
+                step t d u = boundry $ Acc.zipWith (\di st -> integrate t di st) d u 
                 du_start = predict start
-                mid = step (time/2) du_start start  
+                mid = step (time/2) du_start start 
                 du_mid = advance mid
                 final = step time du_mid start 
