f3135905bad5d2ea2aed0e4c25d584dd895fee34
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2018-05-24 13:13:47 +1000

    update for accelerate*-1.3

--- app/prof/Main.hs
+++ app/prof/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns    #-}
+{-# LANGUAGE TemplateHaskell #-}
 --Copyright 2018 GeneralFusion Inc.
 --
 --Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,27 +13,41 @@
 --WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 --See the License for the specific language governing permissions and
 --limitations under the License.
-{-# LANGUAGE TemplateHaskell #-}
-import Sim
-import VAC.Physics.MHD as MHD
-import VAC.Core.Types 
 
-import Data.List as L
-import Data.Array.Accelerate as Acc
+import Sim
+import VAC.Physics.MHD                                    as MHD
+import VAC.Core.Types
+
+import Data.Array.Accelerate                              as A
 import Data.Array.Accelerate.Linear
-import Data.Array.Accelerate.LLVM.PTX as PTX 
+import Data.Array.Accelerate.LLVM.PTX                     as PTX
+-- import Data.Array.Accelerate.LLVM.Native                  as CPU
+
+import Prelude                                            as P
+
 
 startcond :: Array DIM3 MHD
 startcond = $(PTX.runQ initial)
+-- startcond = CPU.runN initial
 
 cell :: Array DIM3 (Cell V3)
 cell = $(PTX.runQ voxels)
+-- cell = CPU.runN voxels
 
 step :: Array DIM3 (Cell V3) -> Array DIM3 MHD -> Array DIM3 MHD
 step = $(PTX.runQ testsimulator)
+-- step = CPU.runN testsimulator
 
 values :: Array DIM3 MHD
-values = L.foldl' (\p _ -> step cell p) startcond [1..100]
+values = loop 0 startcond
+  where
+    lIMIT = 100
+
+    loop :: Int -> Array DIM3 MHD -> Array DIM3 MHD
+    loop !i !p
+      | i P.< lIMIT = loop (i+1) (step cell p)
+      | otherwise   = p
 
 main :: IO ()
 main = putStrLn . show $ indexArray values (Z:.0:.0:.0)
+
